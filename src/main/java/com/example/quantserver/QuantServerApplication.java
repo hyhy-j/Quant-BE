8347ac78e0696bade800775f5088dc13c8898cf0
hyhy-j <lhjlhj10088@gmail.com>
2026-05-23 16:21:57 +0900

    chore: initial project setup

---
+++ src/main/java/com/example/quantserver/QuantServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.quantserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuantServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuantServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/quantserver/QuantServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quantserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuantServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
