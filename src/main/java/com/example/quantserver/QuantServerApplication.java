6f873674b06350b6d519450f7c9fd4642692de71
hyhy-j <lhjlhj10088@gmail.com>
2026-05-23 17:53:35 +0900

    feat: User 도메인 추가

--- src/main/java/com/example/quantserver/QuantServerApplication.java
+++ src/main/java/com/example/quantserver/QuantServerApplication.java
@@ -3,9 +3,11 @@ package com.example.quantserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableJpaAuditing
 public class QuantServerApplication {
 
     public static void main(String[] args) {
