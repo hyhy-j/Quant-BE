6dadef62100d571e9784e5c911d080e65fc81161
hyhy-j <lhjlhj10088@gmail.com>
2026-05-23 23:27:34 +0900

    refactor: Jackson 3 JsonMapper로 마이그레이션

--- src/main/java/com/example/quantserver/global/config/RedisConfig.java
+++ src/main/java/com/example/quantserver/global/config/RedisConfig.java
@@ -1,6 +1,5 @@
 package com.example.quantserver.global.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -9,11 +8,6 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
-
-    @Bean
-    public ObjectMapper objectMapper() {
-        return new ObjectMapper();
-    }
 
     @Bean
     public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
