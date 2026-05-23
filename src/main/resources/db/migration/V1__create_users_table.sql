6ee188b5d2b1fb3e4c1fa56bef722484d9683518
hyhy-j <lhjlhj10088@gmail.com>
2026-05-23 23:38:37 +0900

    chore: Flyway 도입 및 초기 마이그레이션 추가

---
+++ src/main/resources/db/migration/V1__create_users_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE users
+(
+    id         BIGSERIAL PRIMARY KEY,
+    email      VARCHAR(255) NOT NULL,
+    password   VARCHAR(255) NOT NULL,
+    nickname   VARCHAR(255) NOT NULL,
+    role       VARCHAR(50)  NOT NULL,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP
+);
